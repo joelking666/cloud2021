f5c7e4bc9d46e0001e4e381e14c2425c1a113a32
jinhw <2538842834@qq.com>
2021-06-07 21:36:37 +0800

    抽取公共类

---
+++ cloud-api-commons/src/main/java/com/atguigu/springcloud/entities/Payment.java
@@ -0,0 +1,23 @@
+package com.atguigu.springcloud.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @description:
+ * @title: Payment
+ * @Author jinhw
+ * @Date: 2021/6/6 20:50
+ * @Version 1.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment implements Serializable {
+    private Long id;
+    private String serial;
+
+}
